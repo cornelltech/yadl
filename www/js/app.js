44657449267d0d14ff266ba413d8e77d91dac191
AVatch <avatchinsky@outlook.com>
2015-10-28 00:56:42 -0400

    ionic blank boilerplate with dependencies

---
+++ www/js/app.js
@@ -0,0 +1,32 @@
+angular.module('yadl', ['ionic', 'ui.router', 'LocalStorageModule'])
+
+.run( function( $ionicPlatform ) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config( $function(stateProvider, $urlRouterProvider ) {
+  $stateProvider
+    .state('auth', {
+      url: '/auth',
+      templateUrl: 'scripts/accounts/views/auth.tmpl.html',
+    })
+    .state('config', {
+      url: '/config',
+      templateUrl: 'scripts/config/views/config.tmpl.html',
+    })
+    .state('stream', {
+      url: '/stream',
+      templateUrl: 'scripts/stream/views/stream.tmpl.html',
+    })
+    
+  $urlRouterProvider.otherwise('/auth');
+})
